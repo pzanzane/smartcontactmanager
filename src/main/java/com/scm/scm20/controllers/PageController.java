3a2238cf0aaa34d3fe6faf5cc7664e6d585a1ad7
Pankaj Zanzane <pankajzanzane@Pankajs-MacBook-Air.local>
2024-07-29 09:44:00 +0530

    Using Fragments in thymeleaf. Replacing and Inserting Html in fragments. Conditional fragments using query parameter.

--- src/main/java/com/scm/scm20/controllers/PageController.java
+++ src/main/java/com/scm/scm20/controllers/PageController.java
@@ -2,7 +2,11 @@ package com.scm.scm20.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -12,5 +16,20 @@ public class PageController {
         System.out.println("Hello Logs");
         model.addAttribute("name", "SubString Value");
         return "hello";
+    }
+
+    // about page
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public String about(@RequestParam("isLoggedIn") boolean isLoggedIn, Model model) {
+        model.addAttribute("isLoggedIn", isLoggedIn);
+        System.out.println("About Logs");
+        return "about";
+    }
+
+    // services page
+    @RequestMapping("/services")
+    public String services(Model model) {
+        System.out.println("Services Logs");
+        return "services";
     }
 }
